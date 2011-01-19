792c973cf28cbabb6978c6fe860d8dcc68dc89fc
dangtrinhnt <dangtrinhnt@97ae7aed-7466-537e-f450-a170c349389e>
2011-01-19 18:21:02 +0000

    // initiate RegisterActivity

---
+++ AndroidContact/src/com/dt/activities/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.dt.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register_activity);
+		
+	}
+}
