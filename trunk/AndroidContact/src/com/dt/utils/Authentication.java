3c3dcdc4cd8e35f92cd22814b8e0b9aef6f05272
dangtrinhnt <dangtrinhnt@97ae7aed-7466-537e-f450-a170c349389e>
2011-01-20 13:41:35 +0000

    //modify doLogin method to get response string exactly

--- trunk/AndroidContact/src/com/dt/utils/Authentication.java
+++ trunk/AndroidContact/src/com/dt/utils/Authentication.java
@@ -6,7 +6,7 @@ public class Authentication {
 
 	public static boolean doLogin(String loginUrl, String username,
 			String password) {
-		// boolean isSuccess = false;
+		boolean isSuccess = false;
 
 		HttpResponse response = com.dt.http.HttpSender.doPost(loginUrl,
 				username, password);
@@ -14,13 +14,23 @@ public class Authentication {
 		String responseString = com.dt.http.HttpSender
 				.getResponseContent(response);
 
-		if (responseString == "1") {
+		if (responseString.substring(0, 0) == "1") {
+			isSuccess = true;
+		} else {
+			isSuccess = false;
+		}
+
+		return isSuccess;
+
+	}
+
+	public static boolean matchingPassword(String inputPassword,
+			String inputConfirmPassword) {
+		if (inputPassword.equalsIgnoreCase(inputConfirmPassword)) {
 			return true;
 		} else {
 			return false;
 		}
-
-		// return isSuccess;
 
 	}
 
