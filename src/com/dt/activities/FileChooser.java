5f75f16deb474fbf7abe6880caf071daa617d8fd
tanh1989 <tanh1989@gmail.com>
2011-01-20 18:00:42 +0000

    Classes use for read files!

---
+++ src/com/dt/activities/FileChooser.java
@@ -0,0 +1,84 @@
+package com.dt.activities;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import java.util.List;
+
+import com.dt.utils.FileArrayAdapter;
+import com.dt.utils.Option;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class FileChooser extends ListActivity {
+	private File currentDir;
+	private FileArrayAdapter adapter;
+	private List<String> selectedFiles = new ArrayList<String>();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		currentDir = new File("/sdcard/");
+		fill(currentDir);
+	}
+
+	private void fill(File f) {
+		File[] dirs = f.listFiles();
+		this.setTitle("Current Dir: " + f.getName());
+		List<Option> dir = new ArrayList<Option>();
+		List<Option> fls = new ArrayList<Option>();
+		try {
+			for (File ff : dirs) {
+				if (ff.isDirectory())
+					dir.add(new Option("/"+ff.getName(), " ", ff
+							.getAbsolutePath()));
+				else {
+					fls.add(new Option(ff.getName(), "File Size: "
+							+ ff.length()+" KB", ff.getAbsolutePath()));
+				}
+			}
+		} catch (Exception e) {
+		}
+		Collections.sort(dir);
+		Collections.sort(fls);
+		dir.addAll(fls);
+		if (!f.getName().equalsIgnoreCase("sdcard"))
+			dir.add(0, new Option("..", "Parent Directory", f.getParent()));
+		adapter = new FileArrayAdapter(FileChooser.this, R.layout.list_view_layout , dir);
+		this.setListAdapter(adapter);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		Option o = adapter.getItem(position);
+		if ((o.getData().equalsIgnoreCase(" "))
+				|| o.getData().equalsIgnoreCase("parent directory")) {
+			currentDir = new File(o.getPath());
+			fill(currentDir);
+		} else {
+			onFileClick(o);
+			if (!selectedFiles.contains(o.getPath()))
+			{
+				selectedFiles.add(o.getPath());
+			}
+			l.setItemChecked(position, true);
+		}
+	}
+	
+	private void onFileClick(Option o) {
+		Toast.makeText(this, "File Selected: " + o.getName(),
+						Toast.LENGTH_SHORT).show();
+	}
+	/* Creates the menu items */
+    public boolean onCreateOptionsMenu(Menu menu) {    
+    	menu.add(0, 1, 0, "Show the selected files");        
+    	return true;
+    }
+}
---
+++ src/com/dt/utils/FileArrayAdapter.java
@@ -0,0 +1,81 @@
+package com.dt.utils;
+
+import java.util.List;
+
+import com.dt.activities.R;
+
+import android.content.Context;
+
+import android.view.LayoutInflater;
+
+import android.view.View;
+
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import android.widget.TextView;
+
+public class FileArrayAdapter extends ArrayAdapter<Option> {
+
+	private Context c;
+
+	private List<Option> items;
+
+	public FileArrayAdapter(Context context, int textViewResourceId,
+
+	List<Option> objects) {
+
+		super(context, textViewResourceId, objects);
+
+		c = context;
+
+		items = objects;
+
+	}
+
+	public Option getItem(int i)
+
+	{
+
+		return items.get(i);
+
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		View v = convertView;
+
+		if (v == null) {
+
+			LayoutInflater vi = (LayoutInflater) c
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			v = vi.inflate(R.layout.list_view_layout, null);
+
+		}
+
+		final Option o = items.get(position);
+
+		if (o != null) {
+
+			TextView t1 = (TextView) v.findViewById(R.id.TextView01);
+
+			TextView t2 = (TextView) v.findViewById(R.id.TextView02);
+
+			if (t1 != null)
+
+				t1.setText(o.getName());
+
+			if (t2 != null)
+
+				t2.setText(o.getData());
+
+		}
+		return v;
+
+	}
+
+}
+
+
